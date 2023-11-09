f2bfb1351886d4ee9e5ae03688a14092a42e56c1
AntoanYosifov <tapaktapxaomi@gmail.com>
2023-11-09 06:10:51 +0100

    StackOfStrings_04(ExceptionThrowing);

--- src/inheritanceLab/stackOfStrings_04/StackOfStrings.java
+++ src/inheritanceLab/stackOfStrings_04/StackOfStrings.java
@@ -2,6 +2,7 @@ package inheritanceLab.stackOfStrings_04;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class StackOfStrings {
     private final List<String> data;
@@ -15,12 +16,18 @@ public class StackOfStrings {
     }
 
     public String pop() {
+        if(isEmpty()){
+            throw new NoSuchElementException("Cannot invoke .pop() on empty StackOfStrings");
+        }
         int index = this.size() - 1;
 
         return this.data.remove(index);
     }
 
     public String peek() {
+        if(isEmpty()){
+            throw new NoSuchElementException("Cannot invoke .peek() on empty StackOfStrings");
+        }
         int index = this.data.size() - 1;
 
         return this.data.get(index);
